2a3695e9a13b4828115b2ba4a5e18bb58791bf34
tls9603-web <tls9603@gmail.com>
2025-12-03 12:58:18 +0900

    Add : SJYActor

---
+++ SJYActor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SJYActor.h"
+
+// Sets default values
+ASJYActor::ASJYActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASJYActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASJYActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
