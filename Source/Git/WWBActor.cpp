6f4d3d7819d66e341f1b1145d474581661a3562d
zerg8474 <zerg8474@gmail.com>
2025-12-03 12:55:13 +0900

    1

---
+++ Source/Git/WWBActor.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "WWBActor.h"
+
+AWWBActor::AWWBActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AWWBActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
