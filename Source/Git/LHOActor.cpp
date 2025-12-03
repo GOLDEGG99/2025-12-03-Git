cab5ef924e668acc7373f25137fe781f28efedd3
tls9603-web <tls9603@gmail.com>
2025-12-03 16:27:56 +0900

    fix : 팀원 액터 수정

--- Source/Git/LHOActor.cpp
+++ Source/Git/LHOActor.cpp
@@ -9,6 +9,15 @@ ALHOActor::ALHOActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
+	UE_LOG(LogTemp, Warning, TEXT("Nuclear Launch Officer Actor Created"));
+	UE_LOG(LogTemp, Warning, TEXT("Target Acquisition: Confirmed"));
+	UE_LOG(LogTemp, Warning, TEXT("WARNING! This is not a drill"));
+	UE_LOG(LogTemp, Warning, TEXT("5min to launch"));
+	UL_LOG(LogTemp, Warning, TEXT("3min to launch"));
+	UE_LOG(LogTemp, Warning, TEXT("1min to launch"));
+	UE_LOG(LogTemp, Warning, TEXT("10 seconds to launch. launch sequence unstoppable"));
+	UE_LOG(LogTemp, Warning, TEXT("Launch in 5... 4... 3... 2... 1..."));
+	UE_LOG(LogTemp, Warning, TEXT("LAUNCH AUTHORIZED"))
 }
 
 // Called when the game starts or when spawned
